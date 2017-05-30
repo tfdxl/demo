86e8d84a6b342350d549adb660385daab9c5f605
tianfeng <tianfeng@souche.com>
2017-05-30 20:00:47 +0800

    add

---
+++ src/main/java/com/example/service/EmailSendService.java
@@ -0,0 +1,8 @@
+package com.example.service;
+
+/**
+ * Created by tianfeng on 2017/4/30.
+ */
+public interface EmailSendService {
+    boolean sendMail();
+}
---
+++ src/main/java/com/example/service/impl/EmailSendServiceImpl.java
@@ -0,0 +1,15 @@
+package com.example.service.impl;
+
+import com.example.service.EmailSendService;
+
+/**
+ * Created by tianfeng on 2017/4/30.
+ */
+public class EmailSendServiceImpl implements EmailSendService{
+
+
+    @Override
+    public boolean sendMail() {
+        return false;
+    }
+}
