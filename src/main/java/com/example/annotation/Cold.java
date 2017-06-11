29e1db9d5f3e87f824655365639e1e5bc1dfcbf8
tianfeng <tianfeng@souche.com>
2017-06-11 16:13:55 +0800

    add

---
+++ src/main/java/com/example/annotation/Cold.java
@@ -0,0 +1,18 @@
+package com.example.annotation;
+
+import org.mapstruct.Qualifier;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by tianfeng on 2017/6/10.
+ */
+@Target({ElementType.CONSTRUCTOR,ElementType.FIELD,ElementType.METHOD,ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Qualifier
+public @interface Cold {
+    //---》@Qualifier("cold")
+}
