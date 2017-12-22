07ce6f4d608c65e42226ea546803cdd9f5aaf125
tianfeng <tianfeng@souche.com>
2017-12-22 17:10:35 +0800

    add

---
+++ src/main/java/com/example/application/MyApplication.java
@@ -0,0 +1,16 @@
+package com.example.application;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @author tianfeng
+ * @date 2017/12/22
+ */
+public class MyApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }
+}
