6cd7942e552fb92ca9d7dad5f0d16bf552dfea47
tianfeng <tianfeng@souche.com>
2018-01-27 12:14:59 +0800

    add

--- src/main/java/com/example/service/impl/MenuServiceImpl.java
+++ src/main/java/com/example/service/impl/MenuServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class MenuServiceImpl implements MenuService{
 
-    @Cacheable("menu")
+    @Cacheable(value = "menu")
     @Override
     public Menu getMenu(Long id) {
         System.err.println("not from cache ... ");
